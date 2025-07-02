c07ca493c0d5fe6370373e53d50e3e3dc6494b53
Farshad <m_h_faezi@yahoo.com>
2025-07-02 16:21:07 +0200

    Refactored

--- mmto.py
+++ mmto.py
@@ -1,86 +1,95 @@
 """We improved parameters in this version"""
 
-import mtolib.main as mto
 import os
 import glob
 import higra as hg
 import numpy as np
 from PIL import Image
 import mdmto
-import operator
-from helpers import helper
-
+import helper
+import background
 
 """Example program - using original settings"""
 trees, latitudes, longitudes, mu_list, graphs, depths, ids, areas, moments = [], [], [], [], [], [], [], [], []
 
 for fit_file in glob.glob(os.path.join('./data', '*.fits')):
 
-    # Get the input image and parameters
-    image, params = mto.setup(fit_file)
+    image, header = helper.read_image_data(fit_file)  # use fit_file here
+    image = helper.image_value_check(image)
+    image_processed = helper.smooth_filter(image, 2)
 
-    # Pre-process the image
-    processed_image = mto.preprocess_image(image, params, n=2)
+    bg_mean, bg_var, bg_gain = background.estimate_background(image_processed)
+    image_calibrated = image_processed - bg_mean
 
-    # Build a max tree
-    mt = mto.build_max_tree(processed_image, params)
+    graph_structure, tree_structure, altitudes = helper.image_to_hierarchical_structure(image_calibrated)
 
-    # Filter the tree and find objects
-    id_map, sig_ancs = mto.filter_tree(mt, processed_image, params)
+    x, y = helper.centroid(tree_structure, image.shape[:2])
+    distances = np.sqrt((x[tree_structure.parents()] - x) ** 2 + (y[tree_structure.parents()] - y) ** 2)
+    mean, variance = hg.attribute_gaussian_region_weights_model(tree_structure, image_calibrated)
+    area = hg.attribute_area(tree_structure)
+    parent_area = area[tree_structure.parents()]
 
-    # Relabel objects for clearer visualisation
-    id_map = mto.relabel_segments(id_map, shuffle_labels=False)
+    gaussian_intensities = helper.compute_gaussian_profile(
+        mean,
+        variance,
+        distances,
+        altitudes / area
+    )
 
-    '''Revaluing id_map to construct max-tree'''
-    old_id_map = id_map
-    value, counts = np.unique(old_id_map, return_counts=True)
-    tuple_value_count = list(zip(value.tolist(), counts.tolist()))
-    tuple_value_count.sort(key=operator.itemgetter(1))
-    tuple_value_count.reverse()
-    id_map = np.ones(image.shape)
-    id_map = -id_map
+    volume = hg.attribute_volume(tree_structure, altitudes)
+    depth = hg.attribute_depth(tree_structure)
+    first_hu_moment = hg.attribute_moment_of_inertia(tree_structure, graph_structure)
+    parent_altitude = altitudes[tree_structure.parents()]
+    gamma = hg.attribute_topological_height(tree_structure)
+    parent_gamma = gamma[tree_structure.parents()]
 
-    for i in tuple_value_count:
-        if i[0] == -1:
-            continue
-        else:
-            _id = (old_id_map == i[0])
-            id_map[_id] = tuple_value_count.index(i)
-    id_map = id_map.astype(int)
-    # end of revaluing
+    significant_nodes = helper.attribute_statistical_significance(
+        tree_structure, altitudes, volume, area, bg_var, bg_gain
+    )
+    objects = helper.select_objects(tree_structure, significant_nodes)
 
-    # Generate output files
-    mto.generate_image(image, id_map, params)
-    para = mto.generate_parameters(image, id_map, sig_ancs, params)
+    modified_isophote = helper.move_up(
+        tree_structure, altitudes, area, parent_area, distances, objects, bg_var, bg_gain,
+        parent_gamma - gamma, gaussian_intensities, 0, None, .0
+    )
 
-    graph = hg.get_8_adjacency_graph(image.shape)
-    t, a = hg.component_tree_max_tree(graph, id_map)
-    depth = hg.attribute_depth(t)
-    area = hg.attribute_area(t)
-    first_hu_moment = hg.attribute_moment_of_inertia(t, graph)
+    tree_of_segments, n_map_segments = hg.simplify_tree(tree_structure, np.logical_not(modified_isophote))
 
-    # divide area to mean(area) of leaves
-    area[t.num_leaves(): -1] = area[t.num_leaves(): -1] / np.mean(area[t.num_leaves(): -1])
+    segment_ids = np.arange(tree_of_segments.num_leaves(), tree_of_segments.num_vertices())
+    label_data = np.full(tree_of_segments.num_vertices(), -1, dtype=np.int32)
+    label_data[segment_ids] = np.arange(len(segment_ids))
+    seg_array = hg.reconstruct_leaf_data(tree_of_segments, label_data)
+    unique_segment_ids = np.arange(tree_of_segments.num_vertices())[::-1]
 
-    new_mu = helper.total_flux(t, image.shape, processed_image)
-    mu = [-1] * (len(a) - len(new_mu))
-    new_mu = mu + new_mu.tolist()
+    coords_per_segment = [[] for _ in range(len(segment_ids))]
 
-    new_x, new_y = helper.centroid(t, image.shape)
-    x = [-1] * (len(a) - len(new_x))
-    new_x = x + new_x.tolist()
-    y = [-1] * (len(a) - len(new_y))
-    new_y = y + new_y.tolist()
+    for y_ in range(seg_array.shape[0]):
+        for x_ in range(seg_array.shape[1]):
+            label = seg_array[y_, x_]
+            if label >= 0:
+                coords_per_segment[label].append((y_, x_))
 
-    trees.append(t)
-    latitudes.append(new_x)
-    longitudes.append(new_y)
-    mu_list.append(new_mu)
-    graphs.append(graph)
-    depths.append(depth)
-    ids.append(a)
-    areas.append(area)
-    moments.append(first_hu_moment)
+    r_eff = [helper.half_light_radius(image, coords) for coords in coords_per_segment]
+    r_fwhm = [helper.compute_r_fwhm(image, coords) for coords in coords_per_segment]
+
+    centroids = [helper.weighted_centroid_coords_from_segments(image, coords) for coords in coords_per_segment]
+
+    y = [cen[0] for cen in centroids]
+    x = [cen[1] for cen in centroids]
+    ra, dec = helper.sky_coordinates(y, x, header)
+
+    a, b, theta = helper.second_order_moments(tree_of_segments, image.shape[:2], image)
+    flux = hg.accumulate_sequential(tree_of_segments, image, hg.Accumulators.sum)
+
+    trees.append(tree_of_segments)
+    latitudes.append(x[::-1])
+    longitudes.append(y[::-1])
+    mu_list.append(flux[tree_of_segments.num_leaves():][::-1])
+    graphs.append(graph_structure)
+    depths.append(depth[tree_of_segments.num_leaves():][::-1])
+    ids.append(unique_segment_ids[tree_of_segments.num_leaves():][::-1])
+    areas.append(area[n_map_segments][tree_of_segments.num_leaves():][::-1])
+    moments.append(first_hu_moment[tree_of_segments.num_leaves():][::-1])
 
 tree_map = mdmto.tree_map(*trees, *mu_list, *latitudes, *longitudes, *depths, *ids, *areas, *moments)
 
@@ -97,6 +106,6 @@ colors = np.random.randint(
 )
 colors[fusion_tree.root(), :] = 0
 seg = hg.reconstruct_leaf_data(fusion_tree, colors)
-Image.fromarray(np.flipud(seg)).save("mdmto-out.png")
+Image.fromarray(np.flipud(seg)).save("mmto.png")
 
 
